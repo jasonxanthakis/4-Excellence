26a338838b620fb055be293cb1786bc0388b49ca
jasonxanthakis <iasonas.xanthakis@gmail.com>
2025-07-29 12:38:52 +0100

    Added question dumping to setup.js

--- server/db/setup.js
+++ server/db/setup.js
@@ -1,11 +1,26 @@
 const fs = require('fs');
+const path = require('path');
 require("dotenv").config(); 
 
 const sql = fs.readFileSync(__dirname + '/setup.sql').toString();
 
 const db = require("./connect");
 
-db.query(sql)
-    .then(data => console.log("Set-up complete."))
-    .catch(error => console.log(error));
+async function setUpAll() {
+    await db.query(sql)
+        .then(data => console.log("Set-up complete."))
+        .catch(error => console.log(error));
 
+    const questions = require('./dump-questions.js');
+    filepath = path.join(__dirname, '..', '..', 'server', 'db', 'questions', 'test.json');
+
+    await db.query(questions(filepath))
+        .then(data => {
+            console.log('Database seeded.');
+        })
+        .catch(error => console.log(error));
+
+    db.end();
+};
+
+setUpAll();
