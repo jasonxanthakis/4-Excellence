a1c89e443b5ba0bb51947409e8f9a9195e39c354
jasonxanthakis <iasonas.xanthakis@gmail.com>
2025-07-29 14:00:40 +0100

    Fixed bug: Question Dumping can now handle apostrophes

--- server/db/setup.js
+++ server/db/setup.js
@@ -4,21 +4,25 @@ require("dotenv").config();
 
 const sql = fs.readFileSync(__dirname + '/setup.sql').toString();
 
-const db = require("./connect");
+const db = require("./connect.js");
+const questions = require("./dump-questions.js")
+
+const quizzes = ['ks3_history_questions.json', 'ks3_geography_questions.json'];
 
 async function setUpAll() {
     await db.query(sql)
         .then(data => console.log("Set-up complete."))
         .catch(error => console.log(error));
 
-    const questions = require('./dump-questions.js');
-    filepath = path.join(__dirname, '..', '..', 'server', 'db', 'questions', 'test.json');
+    for (let i in quizzes) {
+        let filepath = path.join(__dirname, '..', '..', 'server', 'db', 'questions', quizzes[i]);
 
-    await db.query(questions(filepath))
-        .then(data => {
-            console.log('Database seeded.');
-        })
-        .catch(error => console.log(error));
+        await db.query(questions(filepath))
+            .then(data => {
+                console.log('Dumping questions.');
+            })
+            .catch(error => console.log(error));
+    };
 
     db.end();
 };
