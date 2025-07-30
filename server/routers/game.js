3f3b987775346ba535dadb791390eebef0505096
jasonxanthakis <iasonas.xanthakis@gmail.com>
2025-07-30 09:42:25 +0100

    Removed extra routers

--- server/routers/game.js
+++ server/routers/game.js
@@ -3,7 +3,7 @@ const { Router } = require("express");
 
 const gameRouter = Router();
 
-// gameRouter.get('/', gameController.getAll);
+// gameRouter.get('/', gameController.getRandomQuestions);  // get 10 random questions
 // gameRouter.post('/:gameID', gameController.startGame);   // reponse should include game details, and in the case of quizzes a selection of 10 random questions
 // gameRouter.post('/:gameID/ended', gameController.endGame);   // req body should include score
 
