bb45f9b33ee2a716026bae831cb1718c327df061
hkayy33 <hkayy33@gmail.com>
2025-07-30 09:16:30 +0100

    Updated app.js User.js

--- server/app.js
+++ server/app.js
@@ -1,17 +1,16 @@
 const express = require('express');
 const userRouter = require('./routers/userRouter');
 const gameRouter = require('./routers/gameRouter');
-
 const app = express();
-app.use(cors());
+
 
 app.use(express.json());
 
-// Routes (exactly as you requested)
+// Routes 
 app.use('/user', userRouter); // user routes: /users/___
 app.use('/game', gameRouter); // game routes: game/_____
 
-// Optional: Basic homepage route
+
 app.get("/", (req, res) => {
   res.status(200).json({
     title: "Educational Quiz Game",
